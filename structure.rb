390995bb538f3826815ec51c36eb1b9a21144520
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-04-11 11:59:49 -0400

    add an MTA class which takes an array of lines upon initializaiton. Add a method to list each line and a method which, when given a line, lists that lines stops

--- structure.rb
+++ structure.rb
@@ -23,17 +23,18 @@ class Line
       station_hash = @line[transfer]
       transfer_stop_number = station_hash[:stop]
       @line.keys.each do |station| 
+    #iterate through stops and subtract transfer point stop from each stop
         station_stop_number = @line[station][:stop]
+    #save the absolute value as the distance from transfer point
         distance = (station_stop_number - transfer_stop_number).abs
-        @line[station][:transfer_point] = {name: transfer, distance: distance}
+    #put in line hash
+        if @line[station][:transfer_point] == nil 
+          @line[station][:transfer_point] = []
+        end 
+        @line[station][:transfer_point].push({name: transfer, distance: distance})
       end  
     end 
-    #iterate through stops and subtract transfer point stop from each stop
-    #save the absolute value as the distance from transfer point
-    #put in line hash
   end
-
-
 
   def stops
     @line.keys.each { |stop| puts stop }
@@ -41,9 +42,37 @@ class Line
 
 end 
 
+class MTA
+  #trains (on initialize) should be an array of lines
+  def initialize(trains)
+    puts "Enter if you dare"
+    @trains = {}
+    trains.each do |line| 
+      train_name = line.line_name 
+      @trains[train_name] = line
+    end 
+  end
+
+  def lines
+    @trains.keys.each { |line| puts line} 
+  end 
+
+  def stop_list(line)
+    line_instance = @trains[line]
+    line_instance.stops
+  end 
+
+
+end 
+
+
 the_l = Line.new("L Train", ["6th Street", "8th Street", "Union Square", "3rd Ave", "1st Ave"], ["Union Square"])
-the_l.stops
-puts the_l.line
+the_n = Line.new("N Train", ["TImes Square", "34th Street", "28th Street", "23rd Street", "Union Square", "8th Street"], ["Union Square"])
+the_6 = Line.new("6 Train", ["Grand Central", "33rd Street", "28th Street", "23rd Street", "Union Square", "Astor Place"], ["Union Square"])
+
+the_suck = MTA.new([the_l, the_n, the_6])
+#the_suck.lines 
+the_suck.stop_list("L Train")
 
 
 #stations is a hash - each stop has a name (key) and a 2nd hash as a value, where each key is a transfer point, and each value the number of stops from that transfer point
